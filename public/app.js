617f638974878c52503bcf078dd9f38ccb08241f
pesherind <pesherind@yandex.ru>
2022-10-24 17:52:01 +0300

    make it better

--- public/app.js
+++ public/app.js
@@ -1,49 +1,52 @@
 import QrScanner from "./core/qr-scanner.min.js";
 try {
     BX24.init(()=>{
-        console.log("Успешно")
+        const video = document.getElementById('qr-video');
+        const videoContainer = document.getElementById('container');
+
+
+
+        function setResult(result) {
+            
+            if(result.data){
+                // let data = JSON.parse(result.data)
+                // let text = `Договор: ${data.contract},\nТип документа: ${data.type},\nОтветственный: ${data.employer},\nКонтрагент: ${data.contragent}\nПодтверждаете документ?`
+                let conf = confirm(result.data)
+                if(conf){
+                    scanner.stop()
+                }
+                
+            }
+        }
+
+        const scanner = new QrScanner(video, result => setResult(result), {
+            highlightScanRegion: true,
+            highlightCodeOutline: true,
+        });
+
+        scanner.start()
+
+        document.getElementById('s').addEventListener('click', ()=>{
+            scanner.start()
+        })
+
+        window.scanner = scanner;
+
+        document.getElementById('send').addEventListener("click", ()=>{
+            showing()
+        })
+
+        function showing(){
+            document.getElementById('wrapper').style.display = 'flex'
+        }
+        function hiding(){
+            document.getElementById('wrapper').style.display = 'none'
+        }
+
+
     })
 } catch (error) {
-    
+    alert("Вы не можете использовать это приложение вне Битрикс24")
+    window.location.href = 'https://google.com'
 }
-    const video = document.getElementById('qr-video');
-    const videoContainer = document.getElementById('container');
-
-
-
-    function setResult(result) {
-        
-        if(result.data){
-            // let data = JSON.parse(result.data)
-            // let text = `Договор: ${data.contract},\nТип документа: ${data.type},\nОтветственный: ${data.employer},\nКонтрагент: ${data.contragent}\nПодтверждаете документ?`
-            let conf = confirm(result.data)
-            if(conf){
-                scanner.stop()
-            }
-            
-        }
-    }
-
-    const scanner = new QrScanner(video, result => setResult(result), {
-        highlightScanRegion: true,
-        highlightCodeOutline: true,
-    });
-
-    scanner.start()
-
-    document.getElementById('s').addEventListener('click', ()=>{
-        scanner.start()
-    })
-
-    window.scanner = scanner;
-
-    document.getElementById('send').addEventListener("click", ()=>{
-        showing()
-    })
-
-    function showing(){
-        document.getElementById('wrapper').style.display = 'flex'
-    }
-    function hiding(){
-        document.getElementById('wrapper').style.display = 'none'
-    }
+    
